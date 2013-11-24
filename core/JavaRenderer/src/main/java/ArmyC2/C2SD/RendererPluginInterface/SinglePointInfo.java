844808fa6031311d5e8a44e718f01eef0a132fb9
Robert Hastings <rhastings@moesol.com>
2013-11-24 11:56:46 -0800

    Added maven pom files which build entire project. Added missing interfaces
    	ISinglePointInfo
    	ISinglePointRenderer
    
    Added missing class
    
    	SinglePointInfo
    
    All methods in SinglePointInfo still need implementations.

---
+++ core/JavaRenderer/src/main/java/ArmyC2/C2SD/RendererPluginInterface/SinglePointInfo.java
@@ -0,0 +1,37 @@
+package ArmyC2.C2SD.RendererPluginInterface;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
+public class SinglePointInfo implements ISinglePointInfo {
+
+	public SinglePointInfo(BufferedImage fill) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SinglePointInfo(BufferedImage image, Point symbolCenterPoint,
+			Rectangle2D symbolBounds) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public BufferedImage getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Point2D getSymbolCenterPoint() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rectangle2D getSymbolBounds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
