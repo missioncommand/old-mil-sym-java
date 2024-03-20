73656283cfd5aa632bf45c79e89730f5b363d3b1
Michael Spinelli <michael.p.spinelli6.civ@army.mil>
2024-03-20 10:14:01 -0400

    LOC fix

--- core/JavaLineArray/src/main/java/JavaTacticalRenderer/clsChannelUtility.java
+++ core/JavaLineArray/src/main/java/JavaTacticalRenderer/clsChannelUtility.java
@@ -550,6 +550,11 @@ public final class clsChannelUtility {
                 return;
             
             double factor = 3;
+            
+            // Line of contact looks bad with small channel corners extending out
+            if (linetype == TacticalLines.LC || linetype == TacticalLines.LC_HOSTILE)
+                factor = 1;
+
 
             clsUtility.GetSegments(pixels2, segments, factor);
             partitions = new ArrayList();
